030bb1810dfb286d1b5585f6a057a6f85647233a
sarab-57 <sarabalmuqati@gmail.com>
2025-05-02 21:41:49 +0300

    Switch to joblib for safer model loading

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st 
 import pickle
 import numpy as np
-
+import joblib
 # Load model
 with open('rf_model.pkl', 'rb') as file:
-    model = pickle.load(file)
+    model = joblib.load('rf_model.pkl')
 
 # Title
 st.title("📞 Telecom Churn Predictor")
